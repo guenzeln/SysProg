a83331dfef68629d0ad3f45b5e697b41660110a5
Nico Guenzel <nico.guenzel@yahoo.de>
2013-11-21 16:55:16 +0100

    dasedsa

--- SysProg/server/login.c
+++ SysProg/server/login.c
@@ -8,40 +8,20 @@
  */
 
 #include "login.h"
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <errno.h>
-#include <pthread.h>
-#include <unistd.h>
-#define MAX_CLIENTS 5
-#define BLOCKSIZE 1024
-char message [64] = "Verbindung hergestellt. Bitte Login-Daten eingeben.";
-char buffer [BLOCKSIZE];
-size_t MESSAGE = 64;
-size_t DATA = 1024;
-ssize_t size;
-static struct sockaddr_in server_in;
 
-pthread_t LOGIN, CLIENT [MAX_CLIENTS];
+int loginThread(int client) {
 
-Login_Init (int port) {
 
-	int create_socket;
-    create_socket = Server_Init(port);
-    Client_Init(create_socket);
-
-	}
+    if (client == 1)
+        return 0;
+    else
+        return -1;
 }
 
-
-void Client_Init(int _create_socket) {
+void ClientInit(int _create_socket) {
 
     socklen_t addrlen;
-    int client_socket[MAX_CLIENTS];
+    int client_socket[MAX_CLIENTS];//asdasdsdasdsad
     int c_init = 0;
 
     while (1) {
@@ -56,10 +36,9 @@ void Client_Init(int _create_socket) {
                         printf("Daten vollstaendig gesendet. Warte auf Login-Request vom Client...");
                         if ((size=read(client_socket[c_init], buffer, DATA-1)) < 0)
                             perror("Daten konnten nicht gelesen werden!");
-                        else {
+                        else
                             buffer[size] = '\0';
-                            printf("Nachricht empfangen: %s\n", buffer+2);
-                        }
+                            printf("Nachricht empfangen: %s\n", buffer);
                     }
                 c_init++;
                 }
@@ -68,14 +47,8 @@ void Client_Init(int _create_socket) {
                 printf("Maximale Anzahl an Clients erreicht!");
     }
 }
-/*while (isTrue == 0) {
-           if (pthread_create(&LOGIN, NULL, &(&loginThread)(socketnr), NULL ) != 0)
-               perror("Login-Thread konnte nicht erzeugt werden: ");
-           else
-               isTrue = 1;
-       }*/
 
-int Server_Init(int _port) {
+int ServerInit(int port) {
 
     int s_socket = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -87,7 +60,7 @@ int Server_Init(int _port) {
     printf("Socket wurde angelegt.\n");
     server_in.sin_family = AF_INET;
     server_in.sin_addr.s_addr = INADDR_ANY;
-    server_in.sin_port = htons(_port);
+    server_in.sin_port = htons(port);
 
     if(bind(s_socket, (struct sockaddr *)&server_in, sizeof(server_in)) == -1) {
         perror("Socket konnte nicht an die Adresse gebunden werden: ");
@@ -97,14 +70,6 @@ int Server_Init(int _port) {
         perror("Fehler beim Eingehen von Verbindungsanfragen: ");
         exit(1);
     }
-    return s_socket;
+
+return s_socket;
 }
-
-/*int loginThread(int client) {
-
-
-    if (client == 1)
-        return 0;
-    else
-        return -1;
-}*/
