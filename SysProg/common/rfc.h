cec10555fe53922825653c63ded45733886d7688
Daniel Lutschinski <d.lutschinski@web.de>
2013-11-25 14:50:02 +0100

    rfc

--- SysProg/common/rfc.h
+++ SysProg/common/rfc.h
@@ -31,47 +31,7 @@ int PORT;
 
 #pragma pack(1)
 
-struct NET_HEADER{
 
-        uint8_t type;
-        uint16_t length;
-};
-typedef struct NET_HEADER NET_HEADER;
-
-struct LOGIN_REQUEST{
-
-		struct NET_HEADER header;
-		char* name;
-};
-typedef struct LOGIN_REQUEST LOGIN_REQUEST;
-
-struct LOGIN_RESPONSE{
-
-        struct NET_HEADER header;
-        uint8_t client_id;
-
-};
-typedef struct LOGIN_RESPONSE LOGIN_RESPONSE;
-
-struct CATALOG_REQUEST{
-
-        struct NET_HEADER header;
-};
-typedef struct CATALOG_REQUEST CATALOG_REQUST;
-
-struct CATALOG_RESPONSE{
-
-        struct NET_HEADER header;
-        char* filename;
-};
-
-struct CATALOG_CHANGE{
-
-        struct NET_HEADER header;
-        char* filename;
-
-};
-typedef struct CATALOG_CHANGE CATALOG_CHANGE;
 
 struct PLAYER{
 
@@ -81,58 +41,6 @@ struct PLAYER{
 
 };
 typedef struct PLAYER PLAYER;
-
-struct PLAYERLIST{
-
-        struct NET_HEADER header;
-        struct PLAYER playerlist [6];
-};
-typedef struct PLAYERLIST PLAYERLIST;;
-
-struct START_GAME{
-
-        struct NET_HEADER header;
-        char* filename;
-
-};
-typedef struct START_GAME START_GAME;
-
-struct QUESTION_REQUEST{
-
-        struct NET_HEADER header;
-};
-typedef struct QUESTION_REQUEST QUESTION_REQUEST;
-
-struct QUESTION{
-
-        struct NET_HEADER header;
-        QuestionMessage qmsg;
-};
-typedef struct QUESTION QUESTION;
-
-struct QUESTION_RESULT{
-
-        struct NET_HEADER header;
-        Question question;
-
-};
-typedef struct QUESTION_RESULT QUESTEION_RESULT;
-
-struct GAME_OVER{
-
-        struct NET_HEADER header;
-        uint8_t rank;
-};
-typedef struct GAME_OVER GAME_OVER;
-
-struct ERROR_WARNING{
-
-        struct NET_HEADER h;
-        uint8_t subtype;
-        char* message;
-
-};
-typedef struct ERROR_WARNING ERROR_WARNING;
 
 struct client_data{
         int clientID;
@@ -154,35 +62,33 @@ struct header {
 	uint8_t type;
 	uint16_t length;
 };
-typedef struct header header;
+typedef struct header HEADER;
 
+struct error {
+	uint8_t subtype;
+	char message [255];
+};
+typedef struct error ERROR;
 
 union packetData{
 	char playername[32];
 	char filename[32];
-	//unsigned int score;
 	uint8_t ID;
+	uint8_t rank;
+	PLAYER playerlist [4];
+	ERROR error;
+	QuestionMessage qmessage;
+	Question question;
 };
-typedef union packetData packetData;
+typedef union packetData PACKET_DATA;
 
 struct packet{
-	struct header head;
-	union packetData data;
+	HEADER head;
+	PACKET_DATA data;
 
 };
-typedef struct packet packet;
+typedef struct packet PACKET;
 
-struct error {
-	uint8_t subtype;
-	char message [];
-};
-typedef struct error error;
-
-struct packetError {
-	header head;
-	error err;
-};
-typedef struct packetError packetError;
 
 #pragma pack(0)
 
